a7a43cade7967f2c2b389febf9e8ef4bce9c49c1
zian <mynameiszian@gmail.com>
2023-09-22 02:06:06 +0800

    chore: add lex error

--- lexer/src/lexer/token.rs
+++ lexer/src/lexer/token.rs
@@ -1,6 +1,11 @@
 use core::convert::TryFrom;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
+pub enum LexError {
+    InvalidToken(String),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Token {
     LeftAngle,
     RightAngle,
@@ -31,11 +36,11 @@ pub enum AtSignIdent {
 }
 
 impl AtSignIdent {
-    pub fn from_str_with_ident(ident: String, args: String) -> Result<Self, String> {
+    pub fn from_str_with_ident(ident: String, args: String) -> Result<Self, LexError> {
         match ident.as_str() {
             "type" => Ok(Self::Type(args.trim().to_string())),
             "include" => Ok(Self::Include(args.trim().to_string())),
-            _ => Err(format!("Unknown @ sign identifier: {}", ident)),
+            _ => Err(LexError::InvalidToken(ident)),
         }
     }
 }
